28553be2bcfaaa39085427a43ca024dbd3ee58a1
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-07-03 23:59:12 -0500

    fixed link for home page button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,26 @@
 import React from "react";
-import headshot from '../images/moodyHead.jpeg'
-import background from '../images/background.jpg'
+import headshot from "../images/moodyHead.jpeg";
+import background from "../images/background.jpg";
 
 export default function Home() {
-
   return (
     <div>
-      <section style={{backgroundImage: `url(${background})`, backgroundPosition: `center`}}>
+      <section
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundPosition: `center`,
+        }}
+      >
         <div id="animationContainer" className="console-container">
           <span id="text"></span>
           <div className="console-underscore" id="console">
             &#95;
           </div>
         </div>
-        <img
-          id="moody-headshot"
-          src={headshot}
-          alt="Andrew Moody."
-        />
+        <img id="moody-headshot" src={headshot} alt="Andrew Moody." />
         <div className="buttons">
           <button className="homeButtons">
-            <a href="/projects" rel="noreferrer">
-              Projects
-            </a>
+            <Link to={"/projects"}>Projects</Link>
           </button>
           <button className="homeButtons">
             <a
