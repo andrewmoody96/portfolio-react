c2da5d41dfaf62ed0601d178e536195840e0b032
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-07-03 23:35:32 -0500

    Fixed link typos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 // Here we destructure our props into their own distinct variables
-export default function withRouter(Header) {
+function Header() {
   // Lets log our our loggedIn variable to see it change in real time
   // console.log('Welcome -> loggedIn', loggedIn);
 
@@ -10,32 +10,31 @@ export default function withRouter(Header) {
     <div id="header">
       <header>
         <div id="headerText">
-          <a href="/">
+          <Link to={""}>
             <h1>A.Moody</h1>
             <h2>A.Moody</h2>
             <p id="headerPara">MERN & Music</p>
-          </a>
+          </Link>
+          {/* <a href="">
+            <h1>A.Moody</h1>
+            <h2>A.Moody</h2>
+            <p id="headerPara">MERN & Music</p>
+          </a> */}
         </div>
         <nav>
           <button className="navButtons">
             <Link to={"/projects"}>Projects</Link>
-            {/* <a href="/projects" rel="noreferrer">
-            </a> */}
           </button>
           <button className="navButtons">
             <Link to={"#footer"}>Contact</Link>
-            {/* <a href="#footer" rel="noreferrer">
-              Contact
-            </a> */}
           </button>
           <button className="navButtons">
-            <Link to={"#footer"}>About Me</Link>
-            {/* <a href="/about" rel="noreferrer">
-              About Me
-            </a> */}
+            <Link to={"/about"}>About Me</Link>
           </button>
         </nav>
       </header>
     </div>
   );
 }
+
+export default withRouter(Header);
